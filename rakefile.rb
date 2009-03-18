2752190d15459511a1644708f86f9e3b38678963
Jack <guitarpoet@gmail.com>
2009-03-18 17:26:33 +0800

    Ready to refactor

--- rakefile.rb
+++ rakefile.rb
@@ -5,30 +5,23 @@ require 'fileutils'
 
 include Rake
 
-ex_path = "/Users/jack/Library/Application Support/Firefox/Profiles/dq92c6iy.development/extensions/"
+ex_path = "/Users/jack/Library/Application Support/Firefox/Profiles/dq92c6iy.development/extensions"
 
 Name = "forumtool"
 Version = "1.0"
+ID = "forumtool@gopha.com.cn"
+Path = "#{ex_path}/#{ID}"
 
-class Xpi < PackageTask
-  def zip_file
-    "#{package_name}.xpi"
-  end
+task :clean do
+	puts "Remove the file at #{Path}"
+	if File.exists? Path
+		FileUtils.rm_r Path
+	end
 end
 
-xpi = Xpi.new(Name, Version){ |p|
-    p.need_zip = true
-    p.package_files.include "**/*"
- }
-
-@@FileName = xpi.zip_file
-
-task :clean => :clobber do
-  FileUtils.rm "#{ex_path}/#{@@FileName}"
-end
-
-task :install => [:package] do
-  FileUtils.cp "pkg/#{@@FileName}", ex_path
+task :install => :clean do
+	puts "Installing file to #{ex_path}"
+	FileUtils.cp_r ".", Path
 end
 
 task :firefox => :install do
